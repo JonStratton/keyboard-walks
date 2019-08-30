26a6d74bb131637deacff683c2801f65044b0d7e
JonStratton <jgstratt@sdf.org>
2019-08-30 18:08:28 -0500

    Added keyboard gen with matrix and direction deltas.

--- keyboard-walks.lisp
+++ keyboard-walks.lisp
@@ -1,6 +1,6 @@
 #!/usr/bin/sbcl --script
 
-(defparameter base-count 3)
+(defparameter base-count 4)
 
 (defun process-char-hash(chars-hash cnt)
   "Foreach key(letter) in chars-hash."
@@ -10,31 +10,46 @@
   "Foreach possile direction. Does it have base-count keys?"
   (dolist (dir '(:n :ne :e :se :s :sw :w :nw))
     (let ((current-list (list)))
-       (setf current-list (get-next-char chars-hash cnt firstchar dir))
+       (setf current-list (get-char-line chars-hash cnt firstchar dir))
        (if (>= (list-length current-list) cnt)
-          (format t "Got: ~D~%" current-list)))))
+          (format t "~D~%" current-list)))))
 
-(defun get-next-char(chars-hash cnt current dir &optional (char-list (list current) char-list-supplied-p ))
+(defun get-char-line(chars-hash cnt current dir &optional (char-list (list current) char-list-supplied-p))
   "if there is a char to the dir under cnt, get it"
   (if char-list-supplied-p
     (push current (cdr (last char-list))))
   (if (and (getf (gethash current chars-hash) dir) (> cnt 1))
-    (setf char-list (get-next-char chars-hash (- cnt 1) (getf (gethash current chars-hash) dir) dir char-list)))
+    (setf char-list (get-char-line chars-hash (- cnt 1) (getf (gethash current chars-hash) dir) dir char-list)))
   char-list)
 
+(defun make-keyboard-offset(keyboard offset)
+  "Convert a keyboard matrix and an direction to offset plist to a hash of directions to characters."
+  (let ((x 0) (y 0) (keyboard-offset (make-hash-table :test 'equal)))
+    (dolist (keyrow keyboard) ; foreach row of keys
+      (dolist (key keyrow) ; foreach key
+        (dolist (dir '(:n :ne :e :se :sw :w :nw)) ; foreach direction
+          (if (and (getf offset dir) ; if we have an offset for that direction
+		   key) ; sticking NIL check here for now
+            (let ((x-offset (+ x (nth 0 (getf offset dir))))
+                  (y-offset (+ y (nth 1 (getf offset dir)))))
+              (if (and (>= x-offset 0) ; and a key in that offset
+                       (>= y-offset 0)
+                       (nth y-offset keyboard)
+                       (nth x-offset (nth y-offset keyboard)))
+                (setf (getf (gethash key keyboard-offset) dir) (nth x-offset (nth y-offset keyboard)))))))
+	 (setq x (+ x 1)))
+      (setf x 0)
+      (setf y (+ y 1)))
+    keyboard-offset))
+
 (defun make-chars-hash()
-   "A hash of chars to keyboard directions to keys"
-   ; Hard coded fake micro keyboard to test with. TODO: maybe do this with an algo shifting each row to the right by .5.
-   ( let ((x (make-hash-table :test 'equal)))
-      (setf (gethash '1 x) '(:e 2 :se "q"))
-      (setf (gethash '2 x) '(:e 3 :se "w" :sw "q" :w 1))
-      (setf (gethash '3 x) '(:se "e" :sw "w" :w 2))
-      (setf (gethash "q" x) '(:ne 2 :e "w" :se "a" :nw 1))
-      (setf (gethash "w" x) '(:ne 3 :e "e" :se "s" :sw "a" :w "q" :nw 2))
-      (setf (gethash "e" x) '(:se "d" :sw "s" :w "w" :nw 3))
-      (setf (gethash "a" x) '(:ne "w" :e "s" :nw "q"))
-      (setf (gethash "s" x) '(:ne "e" :e "d" :w "a" :nw "w"))
-      (setf (gethash "d" x) '(:w "s" :nw "e"))
-      x))
+   "A hash of chars to keyboard directions to keys. TODO: Add caps keyboard and number pad"
+   (let ((main-keyboard '(("`" 1 2 3 4 5 6 7 8 9 0 "-" "=")
+			  (NIL "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "\\")
+			  (NIL "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "\"")
+			  (NIL "z" "x" "c" "v" "b" "n" "m" "," "." "/")))
+	 (main-offset '(:ne (1 -1) :e (1 0) :se (0 1) :sw (-1 1) :w (-1 0) :nw (0 -1))))
+     (make-keyboard-offset main-keyboard main-offset))
+)
 
 (process-char-hash (make-chars-hash) base-count)
