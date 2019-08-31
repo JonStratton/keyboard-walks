1ddb1396a7ed1456166a6ada556935f6df0e9833
JonStratton <jgstratt@sdf.org>
2019-08-30 20:51:21 -0500

    Added more keyboards. Renamed to cooler names. Slight refacting.

--- keyboard-walks.lisp
+++ keyboard-walks.lisp
@@ -22,34 +22,48 @@
     (setf char-list (get-char-line chars-hash (- cnt 1) (getf (gethash current chars-hash) dir) dir char-list)))
   char-list)
 
-(defun make-keyboard-offset(keyboard offset)
+(defun make-keyboard(keyboard-matrix dir-cart-delta)
   "Convert a keyboard matrix and an direction to offset plist to a hash of directions to characters."
-  (let ((x 0) (y 0) (keyboard-offset (make-hash-table :test 'equal)))
-    (dolist (keyrow keyboard) ; foreach row of keys
+  (let ((x 0) (y 0) (keyboard (make-hash-table :test 'equal)))
+    (dolist (keyrow keyboard-matrix) ; foreach row of keys
       (dolist (key keyrow) ; foreach key
         (dolist (dir '(:n :ne :e :se :sw :w :nw)) ; foreach direction
-          (if (and (getf offset dir) ; if we have an offset for that direction
+          (if (and (getf dir-cart-delta dir) ; if we have an offset for that direction
 		   key) ; sticking NIL check here for now
-            (let ((x-offset (+ x (nth 0 (getf offset dir))))
-                  (y-offset (+ y (nth 1 (getf offset dir)))))
-              (if (and (>= x-offset 0) ; and a key in that offset
-                       (>= y-offset 0)
-                       (nth y-offset keyboard)
-                       (nth x-offset (nth y-offset keyboard)))
-                (setf (getf (gethash key keyboard-offset) dir) (nth x-offset (nth y-offset keyboard)))))))
+            (let ((x-next-key (+ x (nth 0 (getf dir-cart-delta dir))))
+                  (y-next-key (+ y (nth 1 (getf dir-cart-delta dir)))))
+              (if (and (>= x-next-key 0) ; and a key in that offset
+                       (>= y-next-key 0)
+                       (nth y-next-key keyboard-matrix)
+                       (nth x-next-key (nth y-next-key keyboard-matrix)))
+                (setf (getf (gethash key keyboard) dir) (nth x-next-key (nth y-next-key keyboard-matrix)))))))
 	 (setq x (+ x 1)))
       (setf x 0)
       (setf y (+ y 1)))
-    keyboard-offset))
+    keyboard))
 
-(defun make-chars-hash()
-   "A hash of chars to keyboard directions to keys. TODO: Add caps keyboard and number pad"
-   (let ((main-keyboard '(("`" 1 2 3 4 5 6 7 8 9 0 "-" "=")
-			  (NIL "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "\\")
-			  (NIL "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "\"")
-			  (NIL "z" "x" "c" "v" "b" "n" "m" "," "." "/")))
-	 (main-offset '(:ne (1 -1) :e (1 0) :se (0 1) :sw (-1 1) :w (-1 0) :nw (0 -1))))
-     (make-keyboard-offset main-keyboard main-offset))
+(defun make-keyboards()
+  "A hash of chars to keyboard directions to keys. TODO: Add caps keyboard and number pad"
+  (let ((lc-matrix '(("`" 1 2 3 4 5 6 7 8 9 0 "-" "=")
+                    (NIL "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "\\")
+                    (NIL "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'")
+                    (NIL "z" "x" "c" "v" "b" "n" "m" "," "." "/")))
+	(uc-matrix '(("~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+")
+		     (NIL "Q" "W" "E" "R" "T" "Y" "U" "I" "O" "P" "{" "}" "|")
+		     (NIL "A" "S" "D" "F" "G" "H" "J" "K" "L" ":")
+		     (NIL "Z" "X" "C" "V" "B" "N" "M" "<" ">" "?")))
+	(main-dir-delta '(:ne (1 -1) :e (1 0) :se (0 1) :sw (-1 1) :w (-1 0) :nw (0 -1)))
+	(num-matrix '((NIL "/(1)" "*(1)" "-(1)")
+		      ("7(1)" "8(1)" "9(1)" "+(1)" ) ; TODO: big plus key...
+		      ("4(1)" "5(1)" "6(1)" "+(2)" )
+		      ("1(1)" "2(1)" "3(1)" )
+		      ("0(1)" "0(2)" ".(1)" ))) ; TODO: bug zero key...
+        (num-dir-delta '(:n (-1 0) :ne (-1 1) :e (0 1) :se (1 1) :s (1 0) :sw (1 -1) :w (0 -1) :nw (-1 -1)))
+        (keyboards (make-hash-table :test 'equal)))
+     (maphash #'(lambda (k v) (setf (gethash k keyboards) v)) (make-keyboard lc-matrix main-dir-delta))
+     (maphash #'(lambda (k v) (setf (gethash k keyboards) v)) (make-keyboard uc-matrix main-dir-delta))
+     ;(maphash #'(lambda (k v) (setf (gethash k keyboards) v)) (make-keyboard num-matrix num-dir-delta))
+     keyboards)
 )
 
-(process-char-hash (make-chars-hash) base-count)
+(process-char-hash (make-keyboards) base-count)
