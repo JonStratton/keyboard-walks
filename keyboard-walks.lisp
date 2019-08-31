0e7b31c7eb9b035c33024c668978889cbb473a47
JonStratton <jgstratt@sdf.org>
2019-08-31 07:41:36 -0500

    Renamed some things for clearity.

--- keyboard-walks.lisp
+++ keyboard-walks.lisp
@@ -2,48 +2,47 @@
 
 (defparameter base-count 4)
 
-(defun process-char-hash(chars-hash cnt)
-  "Foreach key(letter) in chars-hash."
-  (maphash #'(lambda (k v) (starting-point chars-hash cnt k)) chars-hash))
+(defun start-walk-keyboard(keyboards cnt)
+  "Foreach key(letter) in keyboards."
+  (maphash #'(lambda (k v) (starting-point keyboards cnt k)) keyboards))
 
-(defun starting-point(chars-hash cnt firstchar)
+(defun starting-point(keyboards cnt firstchar)
   "Foreach possile direction. Does it have base-count keys?"
   (dolist (dir '(:n :ne :e :se :s :sw :w :nw))
-    (let ((current-list (list)))
-       (setf current-list (get-char-line chars-hash cnt firstchar dir))
+    (let ((current-list (get-char-line keyboards cnt firstchar dir)))
        (if (>= (list-length current-list) cnt)
-          (format t "~D~%" current-list)))))
+          (format t "~D: ~D~%" dir current-list)))))
 
-(defun get-char-line(chars-hash cnt current dir &optional (char-list (list current) char-list-supplied-p))
+(defun get-char-line(keyboards cnt current dir &optional (char-list (list current) char-list-supplied-p))
   "if there is a char to the dir under cnt, get it"
   (if char-list-supplied-p
     (push current (cdr (last char-list))))
-  (if (and (getf (gethash current chars-hash) dir) (> cnt 1))
-    (setf char-list (get-char-line chars-hash (- cnt 1) (getf (gethash current chars-hash) dir) dir char-list)))
+  (if (and (getf (gethash current keyboards) dir) (> cnt 1))
+    (setf char-list (get-char-line keyboards (- cnt 1) (getf (gethash current keyboards) dir) dir char-list)))
   char-list)
 
 (defun make-keyboard(keyboard-matrix dir-cart-delta)
   "Convert a keyboard matrix and an direction to offset plist to a hash of directions to characters."
-  (let ((x 0) (y 0) (keyboard (make-hash-table :test 'equal)))
+  (let ((row 0) (col 0) (keyboard (make-hash-table :test 'equal)))
     (dolist (keyrow keyboard-matrix) ; foreach row of keys
       (dolist (key keyrow) ; foreach key
         (dolist (dir '(:n :ne :e :se :sw :w :nw)) ; foreach direction
           (if (and (getf dir-cart-delta dir) ; if we have an offset for that direction
 		   key) ; sticking NIL check here for now
-            (let ((x-next-key (+ x (nth 0 (getf dir-cart-delta dir))))
-                  (y-next-key (+ y (nth 1 (getf dir-cart-delta dir)))))
-              (if (and (>= x-next-key 0) ; and a key in that offset
-                       (>= y-next-key 0)
-                       (nth y-next-key keyboard-matrix)
-                       (nth x-next-key (nth y-next-key keyboard-matrix)))
-                (setf (getf (gethash key keyboard) dir) (nth x-next-key (nth y-next-key keyboard-matrix)))))))
-	 (setq x (+ x 1)))
-      (setf x 0)
-      (setf y (+ y 1)))
+            (let ((row-next-key (+ row (nth 0 (getf dir-cart-delta dir))))
+                  (col-next-key (+ col (nth 1 (getf dir-cart-delta dir)))))
+              (if (and (>= row-next-key 0) ; and a key in that offset
+                       (>= col-next-key 0)
+                       (nth row-next-key keyboard-matrix)
+                       (nth col-next-key (nth row-next-key keyboard-matrix)))
+                (setf (getf (gethash key keyboard) dir) (nth col-next-key (nth row-next-key keyboard-matrix)))))))
+	 (setq col (+ col 1)))
+      (setf col 0)
+      (setf row (+ row 1)))
     keyboard))
 
 (defun make-keyboards()
-  "A hash of chars to keyboard directions to keys. TODO: Add caps keyboard and number pad"
+  "A hash of chars to keyboard directions to keys. TODO: figure out number pad and its double keys."
   (let ((lc-matrix '(("`" 1 2 3 4 5 6 7 8 9 0 "-" "=")
                     (NIL "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "\\")
                     (NIL "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'")
@@ -57,7 +56,7 @@
 		      ("7(1)" "8(1)" "9(1)" "+(1)" ) ; TODO: big plus key...
 		      ("4(1)" "5(1)" "6(1)" "+(2)" )
 		      ("1(1)" "2(1)" "3(1)" )
-		      ("0(1)" "0(2)" ".(1)" ))) ; TODO: bug zero key...
+		      ("0(1)" "0(2)" ".(1)" ))) ; TODO: big zero key...
         (num-dir-delta '(:n (-1 0) :ne (-1 1) :e (0 1) :se (1 1) :s (1 0) :sw (1 -1) :w (0 -1) :nw (-1 -1)))
         (keyboards (make-hash-table :test 'equal)))
      (maphash #'(lambda (k v) (setf (gethash k keyboards) v)) (make-keyboard lc-matrix main-dir-delta))
@@ -66,4 +65,4 @@
      keyboards)
 )
 
-(process-char-hash (make-keyboards) base-count)
+(start-walk-keyboard (make-keyboards) base-count)
